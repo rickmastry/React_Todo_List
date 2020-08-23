935dbf7438d24d609e28c2f3825c5aa9441ba087
jah rik <jahrik@hotmail.com>
2020-08-23 13:43:07 -0400

    Updates

---
+++ precache-manifest.09a45d9d1488213a25de7887afe808d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e726204c8034b03a8feda12a1aa243f7",
+    "url": "/React_Todo_List/index.html"
+  },
+  {
+    "revision": "f742f3092bfad8d68c31",
+    "url": "/React_Todo_List/static/css/main.b63694f6.chunk.css"
+  },
+  {
+    "revision": "77918935a59dd0c2fd6a",
+    "url": "/React_Todo_List/static/js/2.a13ee699.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_Todo_List/static/js/2.a13ee699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f742f3092bfad8d68c31",
+    "url": "/React_Todo_List/static/js/main.e7c19a64.chunk.js"
+  },
+  {
+    "revision": "9dbcd95d987af4e98251",
+    "url": "/React_Todo_List/static/js/runtime-main.561b58e8.js"
+  }
+]);
